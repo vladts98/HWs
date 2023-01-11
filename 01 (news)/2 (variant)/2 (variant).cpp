2e91366f78fee8c7508ada74a2110703e31e6722
Vladislav Tsvetkov <vaflik1998@gmail.com>
2023-01-12 00:20:07 +0300

    01 hw

---
+++ 01 (news)/2 (variant)/2 (variant).cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <vector>
+#include <variant>
+#include <string>
+
+std::variant<int, std::string, std::vector<int>> get_variant() {
+	std::srand(std::time(nullptr));
+	int random_variable = std::rand() % 3;
+
+	std::variant<int, std::string, std::vector<int>> result;
+	switch (random_variable)
+	{
+	case 0:
+		result = 5;
+		break;
+	case 1:
+		result = "string";
+		break;
+	case 2:
+		result = std::vector<int>{ 1, 2, 3, 4, 5 };
+		break;
+	default:
+		break;
+	}
+	return result;
+}
+
+int main()
+{
+	auto a = get_variant();
+	if (std::holds_alternative<int>(a)) {
+		std::cout << std::get<int>(a) * 2;
+	}
+	else if (std::holds_alternative<std::string>(a)) {
+		std::cout << std::get<std::string>(a);
+	}
+	else if (std::holds_alternative<std::vector<int>>(a))
+		for (auto i : std::get<std::vector<int>>(a)) {
+			std::cout << i << " ";
+		}
+	else {
+		std::cout << " aaaaaaa";
+	}
+}
