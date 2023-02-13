68caf21f0c2a769aea4570101db97981cd23c5ee
Vladislav Tsvetkov <vaflik1998@gmail.com>
2023-02-13 23:06:46 +0300

    hw 7

---
+++ 07 (STL container)/7.1/7.1.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <map>
+#include <algorithm>
+
+template<typename A, typename B>
+std::pair<B, A> flip_pair(const std::pair<A, B>& p)
+{
+	return std::pair<B, A>(p.second, p.first);
+}
+
+template<typename A, typename B>
+std::multimap<B, A> flip_map(const std::map<A, B>& src)
+{
+	std::multimap<B, A> dst;
+	std::transform(src.begin(), src.end(), std::inserter(dst, dst.begin()),
+		flip_pair<A, B>);
+	return dst;
+}
+
+
+
+int main()
+{
+
+	std::string s = "Hello world!!";
+	//std::cin >> s;
+	std::map<char, int> m;
+	for (char const& c : s) {
+		if (m.find(c) == m.end()) {
+			m[c] = 1;
+		}
+		else {
+			m[c]++;
+		}
+	}
+
+	std::multimap<int, char> rm = flip_map(m);
+
+	for (auto iter = rm.rbegin(); iter != rm.rend(); ++iter) {
+		std::cout << iter->second << ": " << iter->first << std::endl;
+	}
+
+
+	return 0;
+}
---
+++ 07 (STL container)/7.2/7.2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <set>
+
+int main()
+{
+	int n;
+	std::cout << "vvedite n ";
+	std::cin >> n;
+	std::set<int> my_set;
+	for (int i = 0, a = 0; i < n; ++i) {
+		std::cout << i + 1 << " ";
+		std::cin >> a;
+		my_set.insert(a);
+	}
+	for (auto a : my_set) {
+		std::cout << a << " ";
+	}
+}
