a9f1eab0af57ebb9e49008101bf3983b369e113e
Vladislav Tsvetkov <vaflik1998@gmail.com>
2023-02-16 23:15:19 +0300

    hw 8

---
+++ 08 (STL)/8.1 vector/8.1 vector.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+template <typename T>
+void uvec(std::vector <T>& vec) {
+	auto it = std::unique(vec.begin(), vec.end());
+	vec.erase(it, vec.end());
+}
+
+int main()
+{
+	std::vector<int> vec{ 1, 1, 2, 5, 6, 6, 1, 2, 4 };
+	for (auto v : vec) {
+		std::cout << v << " ";
+	}
+	std::cout << "\n";
+
+	sort(vec.begin(), vec.end());
+	for (auto v : vec) {
+		std::cout << v << " ";
+	}
+	std::cout << "\n";
+	uvec(vec);
+
+	for (auto v : vec) {
+		std::cout << v << " ";
+	}
+}
+
---
+++ 08 (STL)/8.2 print/8.2 print.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <vector>
+#include <set>
+#include <list>
+
+template<typename T>
+void print_container(T t) {
+    auto i = t.begin();
+    while (i != t.end()) {
+        std::cout << *i << " ";
+        i++;
+    }
+    std::cout << "\n";
+}
+
+
+int main()
+{
+    std::set<std::string> test_set = { "one", "two", "three", "four" };
+    print_container(test_set); // four one three two. помните почему такой порядок? :) // по алфавиту 
+
+    std::list<std::string> test_list = { "one", "two", "three", "four" };
+    print_container(test_list); // one, two, three, four
+
+    std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+    print_container(test_vector); // one, two, three, four
+}
