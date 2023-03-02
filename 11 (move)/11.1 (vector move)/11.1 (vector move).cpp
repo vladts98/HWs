093d23a20f92f5affbf21b87ca44e9bdf8bfcb3a
Vladislav Tsvetkov <vaflik1998@gmail.com>
2023-03-03 00:17:59 +0300

    hw 11

---
+++ 11 (move)/11.1 (vector move)/11.1 (vector move).cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <vector>
+
+template <typename T>
+void move_vectors(T&& one, T&& two) { //тут rvalue
+	two.clear(); // обязательно ли чистить?
+	two = std::move(one); //  а как move отчищает объект который мы перемещаем (one)?
+}
+
+//void move_vectors(t& one, t& two) {		// так тоже работает, тут lvalue
+//	two.clear();
+//	two = std::move(one);
+//}
+
+int main()
+{
+	std::vector <std::string> one = { "test_string1", "test_string2" };
+	std::vector <std::string> two;
+	std::cout << one.size() << " " << &one.at(0) << "\n";
+	std::cout << two.size() << "\n";
+	move_vectors(one, two);
+	std::cout << one.size() << "\n";
+	std::cout << two.size() << " " << &two.at(0) << "\n";
+}
---
+++ 11 (move)/11.2 (big value)/11.2 (big value).cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <string>
+
+class big_integer {
+public:
+	big_integer(std::string&& s_) : s{ s_ } {};
+
+	big_integer(big_integer&& other) noexcept {
+		*this = std::move(other);
+	}
+
+	big_integer& operator=(big_integer& other) {
+		this->s = other.s;
+		return *this;
+	}
+
+	big_integer& operator=(big_integer&& other) noexcept {
+		this->s = std::move(other.s);
+		return *this;
+	}
+
+	big_integer operator+(const big_integer& other) {
+		auto a(std::to_string(std::stoll(this->s) + std::stoll(other.s) ));
+		return a;
+	}
+
+	big_integer operator*(const big_integer& other) {
+		auto a(std::to_string(std::stoll(this->s) * std::stoll(other.s)));
+		return a;
+	}
+
+	std::string s;
+};
+
+
+int main()
+{
+	auto number1 = big_integer("114575");
+	auto number2 = big_integer("78524");
+	auto result = number1 * number2;
+	std::cout << number1.s << " " << number2.s << " " << result.s;
+}
