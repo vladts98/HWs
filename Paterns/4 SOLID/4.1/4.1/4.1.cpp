ecd9e1c2e0d013e093e178fb8814bb0f9addd21e
Vladislav Tsvetkov <vaflik1998@gmail.com>
2023-07-05 23:51:54 +0300

    patern 4

---
+++ Paterns/4 SOLID/4.1/4.1/4.1.cpp
@@ -0,0 +1,81 @@
+﻿#include <fstream>
+
+class Printable
+{
+public:
+    virtual ~Printable() = default;
+
+    virtual std::string printAsHTML() const = 0;
+    virtual std::string printAsText() const = 0;
+    virtual std::string printAsJSON() const = 0; // если захотим добавить еще какие то форматы то надо переделывать класс "интерфейс" - принцип открытости
+};
+
+class Data : public Printable
+{
+public:
+    enum class Format 
+    {
+        kText,
+        kHTML,
+        kJSON
+    };
+
+    Data(std::string data, Format format) // в конструкторе определятся формат, а больше половины функций будут бесполезны "Принцип разделения интерфейса"
+        : data_(std::move(data)), format_(format) {}
+
+    std::string printAsHTML() const override
+    {
+        if (format_ != Format::kHTML) { // с учетом такой реализации лучше сделать одну функцию принт и несколько классов
+            throw std::runtime_error("Invalid format!");
+        }
+        return "<html>" + data_ + "<html/>";
+    }
+    std::string printAsText() const override
+    {
+        if (format_ != Format::kText) {
+            throw std::runtime_error("Invalid format!");
+        }
+        return data_;
+    }
+    std::string printAsJSON() const override
+    {
+        if (format_ != Format::kJSON) {
+            throw std::runtime_error("Invalid format!");
+        }
+        return "{ \"data\": \"" + data_ + "\"}";
+    }
+
+private:
+    std::string data_;
+    Format format_; // нельзя узнать текущий формат
+};
+
+void saveTo(std::ofstream& file, const Printable& printable, Data::Format format)
+{
+    switch (format)
+    {
+    case Data::Format::kText:
+        file << printable.printAsText();
+        break;
+    case Data::Format::kJSON:
+        file << printable.printAsJSON();
+        break;
+    case Data::Format::kHTML:
+        file << printable.printAsHTML();
+        break;
+    }
+}
+
+void saveToAsHTML(std::ofstream& file, const Printable& printable) {
+    saveTo(file, printable, Data::Format::kHTML); 
+}
+
+void saveToAsJSON(std::ofstream& file, const Printable& printable) {
+    saveTo(file, printable, Data::Format::kJSON);
+}
+
+void saveToAsText(std::ofstream& file, const Printable& printable) {
+    saveTo(file, printable, Data::Format::kText);
+}
+
+// в этих функциях мы никак не пользуемся базовым классом
---
+++ Paterns/4 SOLID/4.2/4.2/4.2.cpp
@@ -0,0 +1,50 @@
+﻿#include <fstream>
+
+class Printable
+{
+public:
+	Printable(const std::string& data, const std::string& format) : data_(data), format_(format) {}
+	virtual ~Printable() = default;
+	virtual std::string print() const = 0;
+	std::string format() {
+		return format_;
+	}
+protected:
+	std::string format_; //тут можно создать объект enum или как в оригинале или отдельым классом
+	std::string data_;
+};
+
+
+class kText : public Printable {
+public:
+	kText(const std::string& data) : Printable(data, "kText") {}
+	std::string print() const override {
+		return data_;
+	}
+};
+
+
+class kHTML : public Printable {
+public:
+	kHTML(const std::string& data) : Printable(data, "kHTML") {}
+	std::string print() const override {
+		return "<html>" + data_ + "<html/>";
+	}
+};
+
+class kJSON : public Printable {
+public:
+	kJSON(const std::string& data) : Printable(data, "kJSON") {}
+	std::string print() const override {
+		return "{ \"data\": \"" + data_ + "\"}";
+	}
+};
+
+void saveTo(std::ofstream& file, const Printable& printable)
+{
+	file << printable.print();
+}
+
+void save(std::ofstream& file, const Printable& printable) {
+	saveTo(file, printable);
+}
