fa8ddcb757536226d73b39b6b872e5b10f18f05d
Vladislav Tsvetkov <vaflik1998@gmail.com>
2023-08-14 17:30:33 +0300

    thread 2

--- Threads/2 (race)/2.2 progress bar/2.2 progress bar/2.2 progress bar.cpp
+++ Threads/2 (race)/2.2 progress bar/2.2 progress bar/2.2 progress bar.cpp
@@ -3,55 +3,68 @@
 #include <chrono>
 #include <list>
 #include <mutex>
-#include <source_location>
-#include <conio.h>
 #include <windows.h>
-#include <string>
 #include <sstream>
+#include <source_location>
 
 std::mutex m;
-//short line = 0;
 
-void bar(int number, int t) {
+void bar(int number, int full_time) {
 	auto start = std::chrono::high_resolution_clock::now();
+
+	int percent_section = 10; // процент одной секции
 	std::string ch = "|| ";
-	std::string s = std::to_string(number) + " ";
+	std::string bar_s = "";
+	for (int i = 0; i < 100 / percent_section; i++) {
+		bar_s += ch;
+	}
+	int pos_start_bar = 10;  // позиция начала бара для выравнивания
+
+	std::string info = std::to_string(number) + " ";
 
 	std::unique_lock<std::mutex> lck(m);
 	std::stringstream os;
 	os << std::this_thread::get_id();
-	s += os.str() + " ";
-	short c_line = number;
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), COORD{ 0, c_line });
-	std::cout << s;
+	info += os.str();
+	while (info.length() < pos_start_bar) {
+		info += " ";
+	}
+	short line = number; // строчка на которой будет бар
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), COORD{ 0, line });
+	std::cout << info;
 	lck.unlock();
 
-	for (int i = 0; i < t; i++) {
-		s += ch;
+	int procent = 0;
+	if (full_time == 0)
+		procent = 100;
+	while (procent < 100) {
+		std::chrono::duration<float> duration = std::chrono::high_resolution_clock::now() - start;
+		procent = duration.count() / full_time * 100;
+		int lenth = bar_s.length() * procent / 100;
 		lck.lock();
-		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), COORD{ 0, c_line });
-		std::cout << '\r' << s;
+		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), COORD{ 0, line });
+		std::cout << '\r' << info << bar_s.substr(0, lenth);
 		lck.unlock();
-
-		std::this_thread::sleep_for(std::chrono::seconds(1));
 	}
 	auto end = std::chrono::high_resolution_clock::now();
 	std::chrono::duration<double> d = end - start;
+	info += bar_s;
+	info += "Full time: " + std::to_string(d.count());
 	lck.lock();
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), COORD{ 0, c_line });
-	s += "Full time: " + std::to_string(d.count());
-	std::cout << '\r' << s;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), COORD{ 0, line });
+	std::cout << '\r' << info;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), COORD{ 0, 15 });
 	lck.unlock();
 }
 
 int main()
 {
-	int n = 5; // кол-во потоков
+	int n = 3; // кол-во потоков
 	std::list<std::thread> threads;
-	//line = __LINE__;
 	for (int i = 0; i < n; ++i) {
 		threads.push_back(std::thread(bar, i + 1, rand()%10));
 	}
 	for (auto& th : threads)
 		th.join();
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), COORD{ 0, short(n+1) });
 }
---
+++ Threads/2 (race)/2.3 swap/2.3 swap/2.3 swap.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <mutex>
+#include <thread>
+
+class Data {
+public:
+	int n;
+	std::mutex m;
+	Data(int a) : n{ a } {
+	}
+	~Data() {}
+	Data(const Data& d) {
+		this->m.lock();
+		this->n = d.n;
+	}
+};
+
+void swap_lock(Data& d1, Data& d2) {
+	std::lock(d1.m, d2.m);
+	auto temp = d1.n;
+	d1.n = d2.n;
+	d2.n = temp;
+	std::cout << "swap_lock - OK\n";
+	d1.m.unlock();
+	d2.m.unlock();
+
+};
+
+void swap_scoped(Data& d1, Data& d2) {
+	std::scoped_lock lock(d1.m, d2.m);
+	auto temp = d1.n;
+	d1.n = d2.n;
+	d2.n = temp;
+	std::cout << "swap_scoped - OK\n";
+};
+
+void swap_unique(Data& d1, Data& d2) {
+	std::unique_lock<std::mutex> lock1(d1.m);
+	std::unique_lock<std::mutex> lock2(d2.m);
+	auto temp = d1.n;
+	d1.n = d2.n;
+	d2.n = temp;
+	std::cout << "swap_unique - OK\n";
+};
+
+
+int main()
+{
+	Data d1(1);
+	Data d2(2);
+	std::thread t1(swap_lock, std::ref(d1), std::ref(d2));
+	std::thread t2(swap_scoped, std::ref(d1), std::ref(d2));
+	std::thread t3(swap_unique, std::ref(d1), std::ref(d2));
+	t1.join();
+	t2.join();
+	t3.join();
+	return 0;
+}
