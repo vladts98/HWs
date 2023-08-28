70274f45db043fbe8951c3821c4463f4b9ddc99c
Vladislav Tsvetkov <vaflik1998@gmail.com>
2023-08-28 23:47:18 +0300

    hw thread 3

---
+++ Threads/3 (async)/3.1 (sort)/3.1 (sort)/3.1 (sort).cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <vector>
+#include <future>
+
+void sort(std::vector<int>& v) {
+	auto f = [&v](int k, std::promise<int> prom) mutable {
+		for (int j = k; j < v.size(); ++j) {
+			if (v[k] > v[j]) {
+				k = j;
+			}
+		}
+		prom.set_value(k);
+	};
+
+	for (int i = 0; i < v.size(); ++i) {
+		std::promise<int> prom;
+		std::future<int> ft_ = prom.get_future();
+		auto ft = std::async(std::launch::async ,f, i, std::move(prom));
+
+		int k = ft_.get();
+		if (k != i) {
+			std::swap(v[i], v[k]);
+		}
+	}
+}
+
+int main()
+{
+	std::vector<int> v;
+	for (int i = 0; i < 20; ++i) {
+		v.push_back(rand() % 100);
+	}
+	sort(v);
+	for (auto a : v) {
+		std::cout << a << " ";
+	}
+}
---
+++ Threads/3 (async)/3.2 (for_each)/3.2 (for_each)/3.2 (for_each).cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <vector>
+#include <future>
+
+template <typename it, typename f>
+void p_for(it begin, it end, f func) {
+	auto size = std::distance(begin, end);
+	if (size < 10) {
+		std::for_each(begin, end, func);
+		return;
+	}
+	it mid = begin + size / 2;
+	auto a = std::async(std::launch::async, p_for<it, f>, begin, mid, func);
+	p_for(mid, end, func);
+}
+
+int main()
+{
+	std::vector<int> v;
+	for (int i = 0; i < 1000; ++i) {
+		v.push_back(rand() % 100);
+	}
+	auto lamda = [](int& n) {
+		if (n % 2 == 0) {
+			n = 0;
+		}
+	};
+	p_for(v.begin(), v.end(), lamda);
+	for (auto a : v) {
+		std::cout << a << " ";
+	}
+}
