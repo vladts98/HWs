3d9e082e1f5ed8fd03f6713fab32898d7d1a0b36
Vladislav Tsvetkov <vaflik1998@gmail.com>
2023-09-09 01:12:54 +0300

    hw th 4.2

--- Threads/4 (bad)/Project1/Project1/Shape.h
+++ Threads/4 (bad)/Project1/Project1/Shape.h
@@ -1,29 +1,185 @@
 #pragma once
-#include<string>
-class Shape
-{
-public:
-	static const int line = 0;
-	static const int sqr = 1;
-	static const int cube = 2;
-	static const int circle = 3;
-	static const int cylinder = 4;
-	Shape() = default;
-	Shape(int type, int _x1, int _y1, int _z1, int _x2, int _y2, int _z2, int _x3, int _y3, int _z3, int _x4, int _y4, int _z4, int _x5, int _y5, int _z5, int _x6, int _y6, int _z6, int _x7, int _y7, int _z7, int _x8, int _y8, int _z8);
-	Shape(int type, int _x1, int _y1, double R, double H);
-	int getType() { return type; }
+#include <cmath>
+#include <vector>
+#define from_data_base 15 //предположим это запрос в БД
 
-	int type;
-	int x1 = 0, y1 = 0, z1 = 0,
-		x2 = 0, y2 = 0, z2 = 0,
-		x3 = 0, y3 = 0, z3 = 0,
-		x4 = 0, y4 = 0, z4 = 0,
-		x5 = 0, y5 = 0, z5 = 0,
-		x6 = 0, y6 = 0, z6 = 0,
-		x7 = 0, y7 = 0, z7 = 0,
-		x8 = 0, y8 = 0, z8 = 0;
-	double volume;
+class OBJECT {
+public:
+	OBJECT() {
+		id = from_data_base;
+	}
+	 unsigned int get_id() { return id; }
+private:
+	unsigned int id;
+};
+class dot : public OBJECT{
+public:
+	double x = 0, y = 0, z = 0;
+	virtual void scale_x(double s) {
+		x *= s;
+	}
+	virtual void scale_y(double s) {
+		y *= s;
+	}
+	virtual void scale_z(double s) {
+		z *= s;
+	}
+	virtual void scale(double s) {
+		scale_x(s);
+		scale_y(s);
+		scale_z(s);
+	}
+	virtual void shift(double x_, double y_, double z_) {
+		x += x_;
+		y += y_;
+		z += z_;
+	};
+	dot() {
+		x = 0;
+	}
+	dot(double x, double y, double z) {
+		this->x = x;
+		this->y = y;
+		this->z = z;
+	}
+	dot(dot& d) {
+		this->x = d.x;
+		this->y = d.y;
+		this->z = d.z;
+	}
+	dot& operator = (const dot& d) {
+		this->x = d.x;
+		this->y = d.y;
+		this->z = d.z;
+		return *this;
+	}
+	dot& operator = (const dot&& d) {
+		this->x = d.x;
+		this->y = d.y;
+		this->z = d.z;
+		return *this;
+	}
+	dot(dot&& d) {
+		this->x = d.x;
+		this->y = d.y;
+		this->z = d.z;
+	}
+	~dot() {}
+};
+
+class object_2D : public dot {
+public:
+	double get_square() {
+		return square;
+	}
+	void shift(double x_, double y_, double z_) override {
+		for (auto a : v) {
+			a.shift(x_, y_, z_);
+		}
+	};
+	void scale(double s) override {
+		for (auto a : v) {
+			a.scale(s);
+		}
+	}
+	void scale_x(double s) override {
+		for (auto a : v) {
+			a.scale_x(s);
+		}
+	}
+	void scale_y(double s) override {
+		for (auto a : v) {
+			a.scale_y(s);
+		}
+	}
+	void scale_z(double s) override {
+		for (auto a : v) {
+			a.scale_z(s);
+		}
+	}
+protected:
 	double square;
+	std::vector<dot> v;
+};
+
+class object_3D : public object_2D {
+public:
+	double get_volume() {
+		return volume;
+	}
+	double get_height() {
+		return height;
+	}
+protected:
+	double volume;
 	double height;
-	double radius;
+};
+
+class line : public object_2D {
+public:
+	line(dot d_1, dot d_2) {
+		v.push_back(d_1);
+		v.push_back(d_2);
+	}
+	double lenth() {
+		//тут находим длину лини
+		return 0;
+	}
+};
+
+class sqr : public object_2D {
+public:
+	sqr(dot d1, dot d2, dot d3, dot d4) {
+		v.push_back(d1);
+		v.push_back(d2);
+		v.push_back(d3);
+		v.push_back(d4);
+		line l1(d1, d2);
+		line l2(d2, d3);
+		square = l1.lenth() * l2.lenth();
+	}
+};
+
+class cube : public object_3D {
+public:
+	cube(dot d1, dot d2, dot d3, dot d4, dot d5, dot d6, dot d7, dot d8) {
+		v.push_back(d1);
+		v.push_back(d2);
+		v.push_back(d3);
+		v.push_back(d4);
+		v.push_back(d5);
+		v.push_back(d6);
+		v.push_back(d7);
+		v.push_back(d8);
+		line l1(d1, d2);
+		line l2(d2, d3);
+		line l3(d1, d5);
+		square = l1.lenth() * l2.lenth();
+		volume = square * l3.lenth();
+	}
+};
+
+class circle : public object_2D {
+public:
+	circle(dot d1, double r_) {
+		r = r_;
+		v.push_back(d1);
+		square = 3.14 * r * r;
+	}
+protected:
+	double r;
+};
+
+class cylinder : public object_3D {
+public:
+	cylinder(dot d1, double r_, double H_) {
+		r = r_;
+		v.push_back(d1);
+		H = H_;
+		square = 3.14 * r * r + 2 * r * H;
+		volume = 3.14 * r * r * height;
+	}
+protected:
+	double H;
+	double r;
 };
